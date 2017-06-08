ba8a9e9abd8c05e353e944bd4d48cf2a9ca0a2e9
cetecom <cetecom@LC08L1214>
2017-06-08 13:19:16 -0400

    jQuery commit 3

--- index.php
+++ index.php
@@ -15,16 +15,25 @@ and open the template in the editor.
             <div><label>Usuario:</label><input id="nomusuario" type ="text" name="nomusuario"></div>
              <div><label>Clave:</label><input id="clave" type ="password" name="clave"></div>
             <input id="enviar" type="button" onclick="" value="Enviar">
+            <div> id=""</div>
         </form>
     </body>
+    
     
     <script>
         $(document).ready(function(){
     $("#enviar").click(function(){
        /* $("form").hide();
         alert("Oculataste el formulario 8===D---(TU CARA)" + $("#nomusuario").val());*/
-        if ($("#nomusuario").val()!=="" && $("#clave").val()!=="")
-            $("#frmusuario").submit();   
+        if ($("#nomusuario").val()!=="" && $("#clave").val()!==""){
+            //$("#frmusuario").submit();   
+                $.ajax({url:"Controlador/ValidaUsuario.php"
+                ,type:post
+                ,data{nomusuario:$("#nomusuario").val(),clave:$("#clave").val()}
+                ,success:function(resultado){
+                  $("#mensaje").html(resultado);      
+        }});
+                }
             else 
 	       alert("Debe agregar el usuario y clave");
 
